5019f5cb8329151cf81630fd3d30ebbac28b0d17
plaha303 <111688671+plaha303@users.noreply.github.com>
2025-02-17 15:26:49 +0200

    add product list & product detail api

--- backend_api/core/models.py
+++ backend_api/core/models.py
@@ -19,6 +19,8 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200)
     detail = models.TextField(null=True)
     price = models.FloatField()
