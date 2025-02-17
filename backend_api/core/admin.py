7b5232eec3c5ce0429eb83214425badeb1fca972
plaha303 <111688671+plaha303@users.noreply.github.com>
2025-02-17 21:55:38 +0200

    let's do it again

--- backend_api/core/admin.py
+++ backend_api/core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Vendor)
-admin.site.register(models.ProductCategory)
-admin.site.register(models.Product)
+admin.site.register(models.User)
+# admin.site.register(models.ProductCategory)
+# admin.site.register(models.Product)
