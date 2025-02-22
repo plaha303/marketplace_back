762721cd0d49dbaee375be118a1e6eeece1dbf3d
VetaliiP <pvty36@gmail.com>
2025-02-22 18:54:13 +0100

    Header update
    +fonts
    +logo
    +Menu update

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+function Logo() {
+	return (
+		<div className={`min-w-[200px] min-h-[90px] flex justify-center items-center`}>
+             <img
+                src="/img/Logo.svg"
+                alt="Logo"
+                className="size-full"
+            />
+        </div>
+	);
+}
+
+export default Logo;
