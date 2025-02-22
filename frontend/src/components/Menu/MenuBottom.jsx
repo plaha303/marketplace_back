91ac53c9ba8c6fd38baac8a3e025e3c45916267e
VetaliiP <pvty36@gmail.com>
2025-02-22 10:16:55 +0100

    Header Update

--- frontend/src/components/Menu/MenuBottom.jsx
+++ frontend/src/components/Menu/MenuBottom.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+import styled from '../Menu/Menu.module.css';
+
 function MenuBottom() {
 	return (
-		<div>
-      
-		</div>
+		<nav>
+			<ul style={navList}>
+				<li><a href="#">Головна</a></li>
+				<li><a href="#">Про нас</a></li>
+				<li><a href="#">Послуги</a></li>
+				<li><a href="#">Аукціони</a></li>
+				<li><a href="#">Відгуки</a></li>
+				<li><a href="">Центр допомоги</a></li>
+			</ul>
+		</nav>
 	);
 }
+
+const navList = {
+	listStyle: 'none',
+	display: 'flex',
+	justifyContent: 'center',
+	columnGap: '40px',
+	marginTop: '24px'
+  };
 
 export default MenuBottom;
