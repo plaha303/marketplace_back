33c1f1ccd3f916b74041038d564a196d84a3f4ec
VetaliiP <pvty36@gmail.com>
2025-02-25 13:07:03 +0100

    delete paddine in header

--- frontend/src/layout/Header/Header.jsx
+++ frontend/src/layout/Header/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "../../components/Logo/Logo"
 function Header() {
 	return (
 		<div className="header bg-[#f1e4ff]">
-			<div className="container mx-auto px-[15px]">
+			<div className="container mx-auto">
 				<div className="flex flex-row justofy-center items-center gap-x-[24px] pt-[16px] pb-[16px]">
 					<Logo/>
 					<Menu/>
