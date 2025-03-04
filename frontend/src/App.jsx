1338c99f4c29bb550cbc7e4e729acce47438d435
Dasha <dasha2312s3@gmail.com>
2025-03-04 16:47:37 +0200

    added store for modals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Provider } from "react-redux"
 import Layout from "./layout/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Support from "./pages/Support/Support";
+import { store } from "./store/store";
 
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="/support" element={<Support />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="/support" element={<Support />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</Provider>
 	)
 }
 
