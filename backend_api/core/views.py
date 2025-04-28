7c7fe07bca4ec326b7206e5d7d7216aff8fff1e1
Oleksandr-Kyrychuk <azion2342@gmail.com>
2025-04-28 09:47:09 +0000

    Додано можливість верифікації через посилання

--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -16,6 +16,9 @@ from .serializers import (ProductSerializer, OrderSerializer, UserSerializer,
                           RegisterSerializer, PasswordResetRequestSerializer,
                           PasswordResetConfirmSerializer, VerifyEmailSerializer, LoginSerializer,
                           CartSerializer, ResendVerificationCodeSerializer, OrderItem, CartRemoveSerializer, ReviewSerializer, AuctionBidSerializer, FavoriteSerializer, CategorySerializer, PaymentSerializer, ShippingSerializer, UserProfileSerializer)
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
+from django.contrib.auth.tokens import default_token_generator
 
 User = get_user_model()
 
@@ -200,8 +203,13 @@ class LoginView(generics.GenericAPIView):
         return Response({
             'success': False,
             'errors': serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
-        
+        }, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+
+
+
 class CartAddView(generics.GenericAPIView):
     serializer_class = CartSerializer
     permission_classes = [HasGroupPermission]
@@ -363,3 +371,34 @@ class UserProfileView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         serializer = self.get_serializer(request.user)
         return Response({"success": True, "data": serializer.data})
+
+class ActivateEmailView(APIView):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return Response(
+                {"success": False, "errors": {"detail": "Недійсне посилання"}},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if default_token_generator.check_token(user, token):
+            if not user.is_verified:
+                user.is_verified = True
+                user.is_active = True
+                user.verification_code = None
+                user.verification_expires_at = None
+                user.save()
+                return Response(
+                    {"success": True, "message": "Обліковий запис активовано"},
+                    status=status.HTTP_200_OK
+                )
+            return Response(
+                {"success": True, "message": "Обліковий запис уже активовано"},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {"success": False, "errors": {"detail": "Недійсний токен"}},
+            status=status.HTTP_400_BAD_REQUEST
+        )
