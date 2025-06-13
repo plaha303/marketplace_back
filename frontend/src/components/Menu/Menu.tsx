1544e202bc7ca203dead1fc75e9f0dac99d13bce
Dasha <dasha2312s3@gmail.com>
2025-06-13 19:18:22 +0300

    fixed click outside

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,17 +1,17 @@
 import MenuCatalog from "./MenuCatalog";
 import { AnimatePresence, motion } from "motion/react"
 import MenuLinksSection from "./MenuLinksSection/MenuLinksSection";
-import { MenuLinksSectionProps } from "./type/interfaces";
+import { MenuProps } from "./type/interfaces";
 import { useClickOutside } from "@/utils/helpers/useClickOutside";
 import { useRef } from "react";
 
 
-function Menu({openMenuCatalog, handleOpenMenuCatalog}: MenuLinksSectionProps) {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const catalogRef = useClickOutside<HTMLDivElement>(
-    () => handleOpenMenuCatalog(),
-    [buttonRef as React.RefObject<HTMLElement>]
-  );
+function Menu({openMenuCatalog, handleOpenMenuCatalog}: MenuProps) {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const catalogRef = useClickOutside({
+    onTriggered: () => handleOpenMenuCatalog(),
+    excludeRefs: [buttonRef]
+  });
   return (
     <nav className="menu border-b-1 border-primary-100 py-7">
       <div className="container mx-auto px-4">
@@ -24,7 +24,7 @@ function Menu({openMenuCatalog, handleOpenMenuCatalog}: MenuLinksSectionProps) {
             {openMenuCatalog && (
                 <motion.div
                   key="catalogMenu"
-                  ref={catalogRef}
+                  ref={catalogRef as React.Ref<HTMLDivElement>}
                   initial={{ opacity: 0, y: -5 }}
                   animate={{ opacity: 1, y: 5 }}
                   exit={{ opacity: 0, y: -5 }}
