040e9cbb66f97e4d9386e471c108f5a942b20a70
alex <root@DESKTOP-QNA7NCI.localdomain>
2025-06-23 20:56:03 +0300

    front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,19 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr()
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
   },
-})
+});
