a46b9805cd090c64e897817e6b66888298b627a9
Dasha <dasha2312s3@gmail.com>
2025-06-25 12:54:05 +0300

    added topcategories

---
+++ frontend/src/components/TopCategories/TopCategories.tsx
@@ -0,0 +1,27 @@
+function TopCategories() {
+  return (
+    <div className="top-categories">
+      <div className="container px-4 mx-auto">
+        <div className="top-categories__inner">
+          
+          <div className="top-categories__left flex flex-col">
+            <div className="top-categories__block flex-1">
+              <div className="top-categories__block-inner"></div>
+            </div>
+            <div className="top-categories__block flex-1/2">
+              <div className="top-categories__block-inner"></div>
+            </div>
+            <div className="top-categories__block flex-1/2">
+              <div className="top-categories__block-inner"></div>
+            </div>
+          </div>
+
+          <div className="top-categories__right"></div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopCategories;
