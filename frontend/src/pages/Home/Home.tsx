3c5b329e3f824747192105c0663218629fa7817b
Dasha <dasha2312s3@gmail.com>
2025-06-27 22:56:54 +0300

    new components

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Features from "@/components/Features/Features";
 import HomeBanner from "@/components/HomeBanner/HomeBanner";
 import SliderWithProducts from "@/components/SliderWithProducts/SliderWithProducts";
 import SpecialOffers from "@/components/SpecialOffers/SpecialOffers";
+import Subscription from "@/components/Subscription/Subscription";
 import TopCategories from "@/components/TopCategories/TopCategories";
 
 function Home() {
@@ -14,6 +15,8 @@ function Home() {
 			<TopCategories />
 			<SliderWithProducts title="Хіти продаж" watchAll='/' />
 			<SpecialOffers />
+			<SliderWithProducts title="Товари зі знижками" watchAll='/' />
+			<Subscription />
 		</div>
 	);
 }
