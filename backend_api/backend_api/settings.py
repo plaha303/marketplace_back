d131f36ca1040d20159baa73064870ed8762f856
alex <root@DESKTOP-QNA7NCI.localdomain>
2025-07-05 14:46:49 +0300

    cookies secure

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -288,8 +288,8 @@ SIMPLE_JWT = {
 
 # Налаштування cookie
 SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SAMESITE = 'Strict'   #Strict
-SESSION_COOKIE_SECURE = True  # У продакшні  True для HTTPS
+SESSION_COOKIE_SAMESITE = 'Lax'   #Strict
+SESSION_COOKIE_SECURE = False  # У продакшні  True для HTTPS
 
 
 LOGGING = {
