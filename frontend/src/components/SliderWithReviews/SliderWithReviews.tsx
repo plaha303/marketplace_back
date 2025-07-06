d78e305118a651a76caebbc534ce156cc13ae96c
Dasha <dasha2312s3@gmail.com>
2025-07-06 21:50:57 +0300

    added slider with reviews about platform

--- frontend/src/components/SliderWithReviews/SliderWithReviews.tsx
+++ frontend/src/components/SliderWithReviews/SliderWithReviews.tsx
@@ -13,12 +13,39 @@ function SliderWithReviews({title, watchAll, data}: SliderWithReviewsProps) {
   return (
     <div className="slider-review lg:py-[80px] py-12">
       <div className="container px-4 mx-auto">
-        <div className="slider-review__header lg:mb-16 mb-9 flex items-center justify-between md:flex-row flex-col md:gap-0 gap-4">
+        <div className="slider-review__header lg:mb-16 mb-9 flex md:items-center justify-between md:flex-row flex-col md:gap-0 gap-4">
           <h3 className="m:text-size-h3 text-size-h2 uppercase text-accent-800 font-bold leading-130">{title}</h3>
           <Link to={watchAll} className="font-secondary underline text-size-body-3 leading-100 text-primary-600">дивитися всі</Link>
         </div>
-        <div className="slider-review__body">
-          <BaseSlider>
+        <div className="slider-review__body overflow-hidden sm:mr-0 -mr-4">
+          <BaseSlider
+            spaceBetween={36}
+            navigation={true}
+            slidesPerView={3}
+            className="sliderWithBlocks"
+            breakpoints={{
+              0: {
+                slidesPerView: "auto",
+                spaceBetween: 16,
+              },
+              640: {
+                slidesPerView: 1.5,
+                spaceBetween: 16,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 2.5,
+                spaceBetween: 26,
+              },
+              1280: {
+                slidesPerView: 3,
+                spaceBetween: 36,
+              },
+            }}
+          >
             {data.map(review => (
               <ReviewBlock review={review} />
             ))}
