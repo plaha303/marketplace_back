a99b6679f6709a4ef802b9625d88ccfba0235f5e
alex <root@DESKTOP-QNA7NCI.localdomain>
2025-07-10 10:00:25 +0300

    search vector

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
+    'django.contrib.postgres',
     # Dev app
     'corsheaders',
     'rest_framework',
